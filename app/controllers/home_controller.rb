180c0da4a94ce663cc8dd278c5a3609bf6c591b4
kwai <kfchan1052@yahoo.com>
2021-05-07 23:26:16 +1000

    includes pics of house

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,13 @@
+class HomeController < ApplicationController
+  def index
+  end
+
+  def gallery
+  end
+
+  def contacts
+  end
+
+  def about
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  root "home#index"
+  get "home", to: "home#index"
+
+
+  get 'home/about'
+  get "about", to: "home#about"
+
+  get 'home/gallery'
+  get "gallery", to: "home#gallery"
+
+  get 'home/contacts'
+  get "contacts", to: "home#contacts"
+ 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
